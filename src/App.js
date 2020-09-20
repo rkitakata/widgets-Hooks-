fbb0aedc10ba0c4307bb1b22dcd81113fc1a0661
rkitakata <63924908+rkitakata@users.noreply.github.com>
2020-09-20 16:20:36 +0900

    practice of hooks in React
    build a basic wedget for practiceing the Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header'
 
-function App() {
+const items = [
+  {
+    title: 'what is React',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'why use React?',
+    content: 'React is a favorit JS library among engineers'
+  },
+  {
+    title: 'how do you use React?',
+    content: 'You use react by cerating components'
+  }
+]
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+];
+
+
+
+
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search items={items} />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown 
+        label= "Select a clolor"
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
-  );
-}
+  )
+};
 
-export default App;
+
+
+      {/* <Accordion items={items} /> */}
+      {/* <Search /> */}
+      // <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+      // {showDropdown ?
+      // <Dropdown 
+      // selected={selected} 
+      // onSelectedChange={setSelected}
+      // options={options} 
+      // /> : null
+      // }
+
+      // const showAccordion = () => {
+      //   if (window.location.pathname === '/')
+      //   return <Accordion items={items} />
+      // }
+      
+      // const showList = () => {
+      //   if (window.location.pathname === '/list'){
+      //     return <Search />
+      //   }
+      
+      // }
+      
+      // const showDropdown = () => {
+      //   if(window.location.pathname === 'dropdown'){
+      //     return <Dropdown />
+      //   }
+      // }
+      
+      // const showTrnslate = () => {
+      //   if (window.location.pathname === '/translate'){
+      //     return <Translate />
+      //   }
+      // }
+      
